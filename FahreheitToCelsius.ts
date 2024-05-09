48802ea82b1d1a2fdbc52818250271ab960575a0
Đặng Nghĩa <91610413+DangNghia17@users.noreply.github.com>
2024-05-09 22:56:08 +0700

    feat: add new function

---
+++ FahreheitToCelsius.ts
@@ -0,0 +1,8 @@
+class FahrenheitToCelsius {
+    public FahrenheitToCelsiusConverter(FahrenheitTemp: number): number {
+        const _fahreneithTemp = 0.0;
+        const _celsius = (FahrenheitTemp - 32) * 5 / 9;
+        return _celsius;
+    }
+}
+
